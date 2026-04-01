5c6345b13fbd9fe1ee57c0bd85ff1a513c564528
Alex Olson <alexko22222@gmail.com>
2026-04-01 16:56:31 -0400

    added env file for frontend specification

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -97,7 +97,11 @@ export default function Home() {
     setResult(null);
 
     try {
-      const response = await fetch("http://127.0.0.1:8000/analyze-video/", {
+      const response = await fetch(
+        
+        `${process.env.NEXT_PUBLIC_API_BASE_URL}/analyze-video/`,
+        
+        {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
